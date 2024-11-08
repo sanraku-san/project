005493657777b8f5229626665b76d589e7677080
sanraku-san <tabladavedave10@gmail.com>
2024-11-08 18:22:55 +0800

    first_commit

---
+++ pages/Navigation.jsx
@@ -0,0 +1,13 @@
+export default function Navigation(){
+    return(
+    <nav>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>        
+    )
+}
